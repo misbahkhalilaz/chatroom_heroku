59ba3c98589c4857781c2770d306f3c7bf777402
Misbah Khalil <misbah.khalil.az@gmail.com>
2020-07-12 17:45:53 +0500

    init

--- index.js
+++ index.js
@@ -30,4 +30,4 @@ app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-server.listen(4000, () => console.log("4000"));
+app.listen(4000, () => console.log("4000"));
